eea355ba1ee367fc01eb2cc5f5dbc9e6f16df2ba
132yse <1533540012@qq.com>
2020-06-21 20:08:23 +0800

    simplify matrix

--- src/index.js
+++ src/index.js
@@ -113,7 +113,7 @@ export function create(selector) {
     })
   }
 
-  function drawImage(tex, texWidth, texHeight, dstX, dstY) {
+  function drawImage(tex, texWidth, texHeight, mX, mY) {
     gl.bindTexture(gl.TEXTURE_2D, tex)
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
     gl.enableVertexAttribArray(positionLocation)
@@ -121,111 +121,35 @@ export function create(selector) {
     gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer)
     gl.enableVertexAttribArray(texcoordLocation)
     gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0)
-
-    var matrix = orthographic(0, gl.canvas.width, gl.canvas.height, 0, -1, 1)
-
-    matrix = translate(matrix, dstX, dstY, 0)
-
-    matrix = scale(matrix, texWidth, texHeight, 1)
-
-    gl.uniformMatrix4fv(matrixLocation, false, matrix)
-
+    gl.uniformMatrix4fv(matrixLocation, false, matrix(gl.canvas.width, gl.canvas.height, mX, mY, texWidth, texHeight))
     gl.uniform1i(textureLocation, 0)
-
     gl.drawArrays(gl.TRIANGLES, 0, 6)
   }
   return {
     add,
     draw,
-    gl
+    gl,
   }
 }
-function orthographic(left, right, bottom, top, near, far, dst) {
-  dst = dst || new Float32Array(16)
-
-  dst[0] = 2 / (right - left)
-  dst[1] = 0
-  dst[2] = 0
-  dst[3] = 0
-  dst[4] = 0
-  dst[5] = 2 / (top - bottom)
-  dst[6] = 0
-  dst[7] = 0
-  dst[8] = 0
-  dst[9] = 0
-  dst[10] = 2 / (near - far)
-  dst[11] = 0
-  dst[12] = (left + right) / (left - right)
-  dst[13] = (bottom + top) / (bottom - top)
-  dst[14] = (near + far) / (near - far)
-  dst[15] = 1
-
-  return dst
-}
-function translate(m, tx, ty, tz, dst) {
-  dst = dst || new Float32Array(16)
-
-  var m00 = m[0]
-  var m01 = m[1]
-  var m02 = m[2]
-  var m03 = m[3]
-  var m10 = m[1 * 4 + 0]
-  var m11 = m[1 * 4 + 1]
-  var m12 = m[1 * 4 + 2]
-  var m13 = m[1 * 4 + 3]
-  var m20 = m[2 * 4 + 0]
-  var m21 = m[2 * 4 + 1]
-  var m22 = m[2 * 4 + 2]
-  var m23 = m[2 * 4 + 3]
-  var m30 = m[3 * 4 + 0]
-  var m31 = m[3 * 4 + 1]
-  var m32 = m[3 * 4 + 2]
-  var m33 = m[3 * 4 + 3]
-
-  if (m !== dst) {
-    dst[0] = m00
-    dst[1] = m01
-    dst[2] = m02
-    dst[3] = m03
-    dst[4] = m10
-    dst[5] = m11
-    dst[6] = m12
-    dst[7] = m13
-    dst[8] = m20
-    dst[9] = m21
-    dst[10] = m22
-    dst[11] = m23
-  }
-
-  dst[12] = m00 * tx + m10 * ty + m20 * tz + m30
-  dst[13] = m01 * tx + m11 * ty + m21 * tz + m31
-  dst[14] = m02 * tx + m12 * ty + m22 * tz + m32
-  dst[15] = m03 * tx + m13 * ty + m23 * tz + m33
-
-  return dst
-}
-function scale(m, sx, sy, sz, dst) {
-  dst = dst || new Float32Array(16)
-
-  dst[0] = sx * m[0 * 4 + 0]
-  dst[1] = sx * m[0 * 4 + 1]
-  dst[2] = sx * m[0 * 4 + 2]
-  dst[3] = sx * m[0 * 4 + 3]
-  dst[4] = sy * m[1 * 4 + 0]
-  dst[5] = sy * m[1 * 4 + 1]
-  dst[6] = sy * m[1 * 4 + 2]
-  dst[7] = sy * m[1 * 4 + 3]
-  dst[8] = sz * m[2 * 4 + 0]
-  dst[9] = sz * m[2 * 4 + 1]
-  dst[10] = sz * m[2 * 4 + 2]
-  dst[11] = sz * m[2 * 4 + 3]
-
-  if (m !== dst) {
-    dst[12] = m[12]
-    dst[13] = m[13]
-    dst[14] = m[14]
-    dst[15] = m[15]
-  }
-
-  return dst
+function matrix(r, b, x, y, w, h) {
+  let m = new Float32Array(16)
+  m[0] = 2 / r
+  m[1] = 0
+  m[2] = 0
+  m[3] = 0
+  m[4] = 0
+  m[5] = 2 / -b
+  m[6] = 0
+  m[7] = 0
+  m[8] = 0
+  m[9] = 0
+  m[10] = 1
+  m[11] = 0
+  m[12] = m[0] * x + m[4] * y - 1
+  m[13] = m[1] * x + m[5] * y + 1
+  m[14] = 0
+  m[15] = 1
+  m[0] = w * m[0]
+  m[5] = h * m[5]
+  return m
 }
